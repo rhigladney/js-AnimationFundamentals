7c211cfdf2bbaa4fef52fa65b8b8100b59399d3c
rhigladney <71747665+rhigladney@users.noreply.github.com>
2022-10-24 12:45:26 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-AnimationFundamentals
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-d97reb)
---
+++ index.js
@@ -0,0 +1,24 @@
+// Import stylesheets
+import './style.css';
+
+// Animations are cool! 
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Animations are cool! </h1>`;
+
+background(151, 244, 247);
+noStroke();
+
+// position of the car
+var x = 10;
+
+// car body
+fill(255, 0, 115);
+rect(x, 200, 100, 20);
+rect(x + 15, 178, 70, 40);
+
+// wheels
+fill(77, 66, 66);
+ellipse(x + 25, 221, 24, 24);
+ellipse(x + 75, 221, 24, 24);
+
+draw = function() {}
