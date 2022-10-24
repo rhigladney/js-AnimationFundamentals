739b3c845ff961ef2c24ad3834aa80a8955a832f
rhigladney <71747665+rhigladney@users.noreply.github.com>
2022-10-24 12:48:18 -0700

    1 file modified

--- index.js
+++ index.js
@@ -21,4 +21,19 @@ fill(77, 66, 66);
 ellipse(x + 25, 221, 24, 24);
 ellipse(x + 75, 221, 24, 24);
 
-draw = function() {}
+draw = function() {
+
+  //all lines of code will run on repeat
+  // position of the car
+var x = 10;
+
+// car body
+fill(255, 0, 115);
+rect(x, 200, 100, 20);
+rect(x + 15, 178, 70, 40);
+
+// wheels
+fill(77, 66, 66);
+ellipse(x + 25, 221, 24, 24);
+ellipse(x + 75, 221, 24, 24);
+};
